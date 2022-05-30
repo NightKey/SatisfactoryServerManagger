275d4d34ed494c252564ab965d49228c873fb00c
Night <davidjantho@gmail.com>
2022-05-30 21:51:37 +0200

    Updated to standard

--- bot_core.py
+++ bot_core.py
@@ -1,12 +1,16 @@
-import smdb_api, server_managger
-from logger import logger_class, LEVEL
+import smdb_api
+import server_managger
+from smdb_logger import Logger, LEVEL
 from threading import Thread
 from time import sleep
 from random import randint
 
-logger = logger_class("server_managger_bot.log", level=LEVEL.INFO, log_to_console=True)
-managger = server_managger.managger(server_path="/Data/SatisfactoryDedicatedServer", logger=logger)
+logger = Logger("server_managger_bot.log",
+                level=LEVEL.INFO, log_to_console=True)
+managger = server_managger.managger(
+    server_path="/Data/SatisfactoryDedicatedServer", logger=logger)
 loop_thread: Thread = None
+
 
 def start(msg: smdb_api.Message) -> None:
     if not api.is_admin(msg.sender):
@@ -19,7 +23,9 @@ def start(msg: smdb_api.Message) -> None:
         loop_thread = Thread(target=managger.loop)
         loop_thread.name = "Server managger loop"
         loop_thread.start()
-    api.send_message("Server started successfully." if started else "Server start failed!", msg.sender)
+    api.send_message(
+        "Server started successfully." if started else "Server start failed!", msg.sender)
+
 
 def stop(msg: smdb_api.Message) -> None:
     if not api.is_admin(msg.sender):
@@ -29,13 +35,16 @@ def stop(msg: smdb_api.Message) -> None:
     managger.exit()
     api.send_message("Server stopped!", msg.sender)
 
+
 def update(msg: smdb_api.Message) -> None:
     if not api.is_admin(msg.sender):
         logger.warning("Not admin called the update function!")
         api.send_message("Only admins can use this command!", msg.sender)
         return
     updated = managger.update()
-    api.send_message(("Server restarted successfully!" if managger.loop_running else "Server updated successfully!") if updated else "Server update failed!", msg.sender)
+    api.send_message(("Server restarted successfully!" if managger.loop_running else "Server updated successfully!")
+                     if updated else "Server update failed!", msg.sender)
+
 
 def restart(msg: smdb_api.Message) -> None:
     if not api.is_admin(msg.sender):
@@ -43,20 +52,30 @@ def restart(msg: smdb_api.Message) -> None:
         api.send_message("Only admins can use this command!", msg.sender)
         return
     restart = managger.restart()
-    api.send_message("Server restarted successfully!" if restart else "Server update failed!", msg.sender)
+    api.send_message(
+        "Server restarted successfully!" if restart else "Server update failed!", msg.sender)
+
 
 def is_running(msg: smdb_api.Message) -> None:
-    api.send_message("The server is up and running" if managger.is_running else "The server is stopped", msg.sender)
+    api.send_message(
+        "The server is up and running" if managger.is_running else "The server is stopped", msg.sender)
+
 
 logger.debug(f"Random id: {randint(0,10)}")
 
-api = smdb_api.API("Satisfactory managger", "a4bdb9b345435631ca6b9c093324ee3a76b8322811fcd82ad04f537946ab6e88")
+api = smdb_api.API("Satisfactory managger",
+                   "a4bdb9b345435631ca6b9c093324ee3a76b8322811fcd82ad04f537946ab6e88")
 api.validate()
-api.create_function("SFStart", "Starts the satisfactory server. Admin only command.\nUsage: &SFStart\nCategory: HARDWARE", start)
-api.create_function("SFUpdate", "Updates the satisfactory server. Admin only command.\nUsage: &SFUpdate\nCategory: HARDWARE", update)
-api.create_function("SFStop", "Stops the satisfactory server. Admin only command.\nUsage: &SFStop\nCategory: HARDWARE", stop)
-api.create_function("SFRestart", "Restarts the satisfactory server if it's running, othervise starts a new one. Admin only command.\nUsage: &SFRestart\nCategory: HARDWARE", restart)
-api.create_function("SFStatus", "Returns the satisfactory server's status.\nUsage: &SFStatus\nCategory: HARDWARE", is_running)
+api.create_function(
+    "SFStart", "Starts the satisfactory server. Admin only command.\nUsage: &SFStart\nCategory: HARDWARE", start)
+api.create_function(
+    "SFUpdate", "Updates the satisfactory server. Admin only command.\nUsage: &SFUpdate\nCategory: HARDWARE", update)
+api.create_function(
+    "SFStop", "Stops the satisfactory server. Admin only command.\nUsage: &SFStop\nCategory: HARDWARE", stop)
+api.create_function(
+    "SFRestart", "Restarts the satisfactory server if it's running, othervise starts a new one. Admin only command.\nUsage: &SFRestart\nCategory: HARDWARE", restart)
+api.create_function(
+    "SFStatus", "Returns the satisfactory server's status.\nUsage: &SFStatus\nCategory: HARDWARE", is_running)
 try:
     while True:
         sleep(1)
--- server_managger.py
+++ server_managger.py
@@ -1,32 +1,40 @@
-import subprocess, platform
-from logger import logger_class, LEVEL
+import subprocess
+import platform
+from smdb_logger import Logger, LEVEL
 from typing import List
 from time import sleep, perf_counter
+
 
 class command:
     def __init__(self, string_repr: str) -> None:
         self.string = string_repr
-    
+
     def fill(self, **kwargs) -> "command":
         return command(self.string.format(**kwargs))
-    
+
     def to_cmd(self) -> List[str]:
         return self.string.split(' ')
-    
+
     def __str__(self):
         return self.string
 
+
 SUCCESS = 0
 
+
 class managger:
-    steam_update_command = command("steamcmd +force_install_dir {path} +login {user_data} +app_update 1690800 validate +quit")
+    steam_update_command = command(
+        "steamcmd +force_install_dir {path} +login {user_data} +app_update 1690800 validate +quit")
     default_server_path = "~/SatisfactoryDedicatedServer"
     anonime_user_data = "anonymous"
     default_additionals = "-log -unattended"
     windows_run_command = command("{path}/FactoryServer.exe {additionals}")
-    linux_run_before = command("chmod +x {path}/Engine/Binaries/Linux/UE4Server-Linux-Shipping")
-    linux_run_command = command("{path}/Engine/Binaries/Linux/UE4Server-Linux-Shipping FactoryGame {additionals}")
-    def __init__(self, server_path: str = None, steam_username: str = None, steam_password: str = None, logger: logger_class = None, additionals: str = None) -> None:
+    linux_run_before = command(
+        "chmod +x {path}/Engine/Binaries/Linux/UE4Server-Linux-Shipping")
+    linux_run_command = command(
+        "{path}/Engine/Binaries/Linux/UE4Server-Linux-Shipping FactoryGame {additionals}")
+
+    def __init__(self, server_path: str = None, steam_username: str = None, steam_password: str = None, logger: Logger = None, additionals: str = None) -> None:
         self.user_info: str = f"{steam_username} {steam_password}" if steam_username is not None else managger.anonime_user_data
         self.server_path: str = server_path if server_path is not None else managger.default_server_path
         self.server: subprocess.Popen = None
@@ -36,14 +44,19 @@ class managger:
         self.is_running: bool = False
         self.loop_running = False
         self.additionals = additionals if additionals is not None else managger.default_additionals
-        self.logger = logger if logger is not None else logger_class("satisfactory_server_managger.lg", LEVEL=LEVEL.INFO)
-        self.environment_specific_command: command = managger.windows_run_command if platform.system() == "Windows" else managger.linux_run_command
-        logger.info(f"Server managger created with server path: {self.server_path}")
+        self.logger = logger if logger is not None else logger_class(
+            "satisfactory_server_managger.lg", LEVEL=LEVEL.INFO)
+        self.environment_specific_command: command = managger.windows_run_command if platform.system(
+        ) == "Windows" else managger.linux_run_command
+        logger.info(
+            f"Server managger created with server path: {self.server_path}")
 
     def _update_server(self) -> bool:
         self.logger.info("Updating server")
-        update_command = managger.steam_update_command.fill(user_data=self.user_info, path=self.server_path)
-        self.logger.info(f"Update called with following data: {update_command}")
+        update_command = managger.steam_update_command.fill(
+            user_data=self.user_info, path=self.server_path)
+        self.logger.info(
+            f"Update called with following data: {update_command}")
         try:
             subprocess.check_call(update_command.to_cmd())
             return True
@@ -55,7 +68,8 @@ class managger:
     def start_server(self, update_before: bool = False) -> bool:
         self.logger.info(f"Start called!")
         if self.is_running:
-            self.logger.info("Server start called, while the server is already running!")
+            self.logger.info(
+                "Server start called, while the server is already running!")
             return True
         self.run = True
         if update_before:
@@ -64,7 +78,8 @@ class managger:
         if platform.system() != "Windows":
             run_before = self.linux_run_before.fill(path=self.server_path)
             subprocess.check_call(run_before.to_cmd())
-        start_command = self.environment_specific_command.fill(path=self.server_path, additionals=self.additionals)
+        start_command = self.environment_specific_command.fill(
+            path=self.server_path, additionals=self.additionals)
         self.server = subprocess.Popen(start_command.to_cmd())
         fail_count = 0
         while self.server.poll() is not None:
@@ -77,7 +92,7 @@ class managger:
         self.is_running = True
         self.logger.info("Server started")
         return True
-    
+
     def stop_server(self) -> None:
         if not self.is_running:
             self.logger.info("Stop called while the server is not running!")
@@ -89,19 +104,21 @@ class managger:
         sleep_count = 1
         sleep(60)
         while self.server.poll() is None:
-            self.logger.warning(f"Serve did not terminate after {sleep_count*60} secunds!")
-            if sleep_count >= 2: 
+            self.logger.warning(
+                f"Serve did not terminate after {sleep_count*60} secunds!")
+            if sleep_count >= 2:
                 self.logger.info("Trying to kill the server!")
                 self.server.kill()
             sleep_count += 1
             sleep(60)
         self.logger.info("Server stopped")
         self.is_running = False
-    
+
     def exit(self) -> None:
         self.logger.info("Exiting server managger...")
         self.run = False
-        if self.is_running: self.stop_server()
+        if self.is_running:
+            self.stop_server()
 
     def update(self) -> bool:
         self.logger.info("Update called!")
@@ -113,7 +130,8 @@ class managger:
 
     def restart(self) -> bool:
         self.logger.info("Restart called!")
-        if self.is_running: self.stop_server()
+        if self.is_running:
+            self.stop_server()
         return self.start_server(False)
 
     def loop(self) -> None:
